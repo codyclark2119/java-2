0108511c60d9266204d4f4d3978f0513ba46b001
Cody Clark <codyclark2119@gmail.com>
2020-06-29 18:39:02 -0500

    finished side class and activity 1

--- burger_shop/Side.java
+++ burger_shop/Side.java
@@ -1,4 +1,24 @@
 package burger_shop;
 
 public class Side {
+    private String name;
+    private double price;
+
+    private Side(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public static final Side Fries = new Side("French Fries", 1.50 );
+    public static final Side OnionRings = new Side("Onion Rings", 2.50 );
+    public static final Side FriedMushrooms = new Side("Fried Mushrooms", 2.75);
+
 }
